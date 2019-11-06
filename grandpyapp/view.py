a4e34aa65affcfea92c9a6f5cff72cff0b1f98a4
youpiyoful <yoanfornari@gmail.com>
2019-11-06 18:03:15 +0100

    [wip] valide the test of template render

--- grandpyapp/view.py
+++ grandpyapp/view.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 # Config options - Make sure you created a 'config.py' file.
@@ -7,5 +7,8 @@ GOOGLE_API_KEY = app.config.get('GOOGLE_API_KEY')
 
 
 @app.route('/', methods=['GET'])
+@app.route('/grandpy_bot/', methods=['GET'])
 def index():
-    return 'Hello world'
+    render = render_template('index.html')
+    print(render)
+    return render
