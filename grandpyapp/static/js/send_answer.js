4afa8e449ab22663a04c8753636b58d630846d07
youpiyoful <yoanfornari@gmail.com>
2020-03-31 00:35:12 +0200

    [wip] add xmlhttprequest for send answer

---
+++ grandpyapp/static/js/send_answer.js
@@ -0,0 +1,9 @@
+const sendForm = document.getElementById('send');
+
+const sendForm = () => {
+    const request = new XMLHttpRequest();
+    request.open('GET', 'send_question')
+}
+sendForm.addEventListener("click", (event) => {
+    
+})
