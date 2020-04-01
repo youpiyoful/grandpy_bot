a58ffe7a6172e9a696fbc661cd681ab8ac9cdfae
youpiyoful <yoanfornari@gmail.com>
2020-04-01 14:13:56 +0200

    [refactoring] find_keyword method with list comprehension

--- grandpyapp/parser.py
+++ grandpyapp/parser.py
@@ -1,5 +1,6 @@
 """This file is a class parser use like an helper for parse answer of user"""
 
+
 class Parser:
     """class than use stop word and regex for parse string"""
     def __init__(self, answer, stop_words):
@@ -7,19 +8,15 @@ class Parser:
         self.stop_words = stop_words
 
     # @staticmethod
-    # def string_parse(string):
-    #     return string.split(" ")
+    # def remove_unutil_word(my_list, word):
+    #     return my_list.remove(word)
 
     def find_keyword(self):
         """this fuction take a string and return a list of keyword"""
         list_of_words = self.answer.split(" ")
 
         for stop_word in self.stop_words:
-            
-            for word in list_of_words:
 
-                if word == stop_word:
-                    print('print : ', stop_word, ' -> ', word)
-                    list_of_words.remove(word)
+            [list_of_words.remove(word) for word in list_of_words if word == stop_word]  # use list comprehension
                     
         return list_of_words
