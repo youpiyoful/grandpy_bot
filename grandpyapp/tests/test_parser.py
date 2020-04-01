08fd49cc79c8bb6bb90427c20f353a1807dc645f
youpiyoful <yoanfornari@gmail.com>
2020-04-01 15:58:54 +0200

    [clean] delete unused test code

--- grandpyapp/tests/test_parser.py
+++ grandpyapp/tests/test_parser.py
@@ -1,16 +1,8 @@
 """File who test method of parser class"""
 from grandpyapp.parser import Parser
 
-# class TestParser:
-#     """this class test method of Parser class"""
-#     def __init__(self):
-#         self.parser = Parser("test", ["test"])
-
 PARSER = Parser("ceci est ma phrase", ["ceci", "est", "ma"])
 
-# def test_string_parse():
-#     """this method test than string_parse return a string"""
-#     assert PARSER.string_parse() == "ceci est ma phrase"
 
 def test_find_keyword():
     """test the method who find the keyword in a phrase"""
