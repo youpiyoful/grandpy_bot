2705a24458c757577ee0c694b0b83eaecfadc48b
youpiyoful <yoanfornari@gmail.com>
2020-04-02 12:07:51 +0200

    [fix] fix import pb

--- grandpyapp/tests/test_place.py
+++ grandpyapp/tests/test_place.py
@@ -1,9 +1,11 @@
 """This file test the file class place than use the google place api"""
-from grandpyapp.place import Place
+from grandpyapp import place
+import requests
 
-PLACE = Place("openclassrooms", "an api key", "http://ça_marche_fort_bien.com")
+PLACE = place.Place("openclassrooms", "anApiKey", "http://ça_marche_fort_bien.com")
 
-def test_find_place():
+
+def test_find_place(monkeypatch):
     """this test verify than method return a correct json"""
     google_resp = \
     {
@@ -17,5 +19,18 @@ def test_find_place():
             }
         ]
     }
-    response = PLACE.find_place()
-    assert google_resp["candidates"][0]["formatted_address"] == response["candidates"][0]["formatted_address"]
+
+    def mock_google_api_request(request):
+        return google_resp
+
+    monkeypatch.setattr(place, 'une fonction de request', mock_google_api_request)
+
+    resp = PLACE.find_place()
+    print('response = ', resp)
+    print('google_resp = ', google_resp["candidates"][0]["formatted_address"])
+    # assert google_resp["candidates"][0]["formatted_address"] == response["candidates"][0]["formatted_address"]
+
+
+def test_build_url_for_api_place():
+    """this method take Place's attribut and build an endpoint"""
+    PLACE
