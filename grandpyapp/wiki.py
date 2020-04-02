e9d306ff2901abbf7173422b8ee6c5a317ea8aed
youpiyoful <yoanfornari@gmail.com>
2020-04-02 12:10:45 +0200

    create class wiki and find_data_about_place method

--- grandpyapp/wiki.py
+++ grandpyapp/wiki.py
@@ -1,1 +1,11 @@
 """This file maped an object than used wiki api""" 
+
+
+class Wiki:
+    """this class use wiki api for find data about place"""
+    def __init__(self):
+        pass
+
+    def find_data_about_place(self):
+        """this method call wiki api for find data about place""" 
+        return {'test': 'api wikipedia return'}
