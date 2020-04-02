7720373045062e7bbcc77790f5b3075c32186305
youpiyoful <yoanfornari@gmail.com>
2020-04-02 23:36:01 +0200

    [fix] fix pb of import and return correct json test at the send_answer endpoint

--- grandpyapp/view.py
+++ grandpyapp/view.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, json
-from place import Place
-from wiki import Wiki
-from parser import Parser
+from grandpyapp.place import Place
+from grandpyapp.wiki import Wiki
+from grandpyapp.parser import Parser
 
 
 app = Flask(__name__)
@@ -11,7 +11,6 @@ GOOGLE_API_KEY = app.config.get('GOOGLE_API_KEY')
 WIKI_API_KEY = app.config.get('WIKI_API_KEY')
 BASE_URL_GOOGLE_PLACE = app.config.get('BASE_URL_GOOGLE_PLACE')
 BASE_URL_WIKI = app.config.get('BASE_URL_WIKI')
-
 
 
 @app.route('/', methods=['GET'])
@@ -32,13 +31,13 @@ def index():
 @app.route('/send_answer/<answer>', methods=['GET'])
 def get_data(answer):
     stop_words = []
-    PARSER = Parser(answer, stop_words) # instanciate Parser class
-    keyword = PARSER.find_keyword()  # call method of PARSER object
-    PLACE = Place(keyword, GOOGLE_API_KEY, BASE_URL_GOOGLE_PLACE) # instanciate Place class
-    data_google = PLACE.find_place() # call method of PLACE object
-    WIKI = Wiki() # instanciate wiki class
-    data_wiki = WIKI.find_data_about_place() # call method of WIKI object
-    data = {'data_google': {data_google}, 'data_wiki': {data_wiki}} # concatenate data_google and data_wiki in a big json object
+    parser_obj = Parser(answer, stop_words)  # instanciate Parser class
+    keyword = parser_obj.find_keyword()  # call method of PARSER object
+    place_obj = Place(keyword, GOOGLE_API_KEY, BASE_URL_GOOGLE_PLACE)  # instanciate Place class
+    data_google = place_obj.find_place()  # call method of PLACE object
+    wiki_obj = Wiki()  # instanciate wiki class
+    data_wiki = wiki_obj.find_data_about_place()  # call method of WIKI object
+    data = {'data_google': data_google, 'data_wiki': data_wiki}  # concatenate data_google and data_wiki in a big json object
     print(data)
     response = app.response_class(
         response=json.dumps(data),
@@ -46,11 +45,6 @@ def get_data(answer):
         mimetype='application/json'
     )
     return response
-    # return render_template('index.html',
-    #                        submited,
-    #                        answer)
 
 
 # mieux vaut avoir un gros appell que plusieurs petit
-
-print(get_data("openclassrooms"))
