c1728c3431d728046354928ce730f2f0044fad91
youpiyoful <yoanfornari@gmail.com>
2020-04-02 12:12:11 +0200

    [wip]function get data use now parser, wiki and place classes

--- grandpyapp/view.py
+++ grandpyapp/view.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, json
+from place import Place
+from wiki import Wiki
+from parser import Parser
 
 
 app = Flask(__name__)
 # Config options - Make sure you created a 'config.py' file.
 app.config.from_object('config')
 GOOGLE_API_KEY = app.config.get('GOOGLE_API_KEY')
+WIKI_API_KEY = app.config.get('WIKI_API_KEY')
+BASE_URL_GOOGLE_PLACE = app.config.get('BASE_URL_GOOGLE_PLACE')
+BASE_URL_WIKI = app.config.get('BASE_URL_WIKI')
+
 
 
 @app.route('/', methods=['GET'])
@@ -24,11 +31,26 @@ def index():
 
 @app.route('/send_answer/<answer>', methods=['GET'])
 def get_data(answer):
-    pass
-
+    stop_words = []
+    PARSER = Parser(answer, stop_words) # instanciate Parser class
+    keyword = PARSER.find_keyword()  # call method of PARSER object
+    PLACE = Place(keyword, GOOGLE_API_KEY, BASE_URL_GOOGLE_PLACE) # instanciate Place class
+    data_google = PLACE.find_place() # call method of PLACE object
+    WIKI = Wiki() # instanciate wiki class
+    data_wiki = WIKI.find_data_about_place() # call method of WIKI object
+    data = {'data_google': {data_google}, 'data_wiki': {data_wiki}} # concatenate data_google and data_wiki in a big json object
+    print(data)
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
     # return render_template('index.html',
     #                        submited,
     #                        answer)
 
 
 # mieux vaut avoir un gros appell que plusieurs petit
+
+print(get_data("openclassrooms"))
