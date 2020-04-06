cdffb221444667b2620abffd295a610a794c1c08
youpiyoful <yoanfornari@gmail.com>
2020-04-06 16:33:21 +0200

    [wip] create function for manipulate dom with user event

---
+++ grandpyapp/static/js/dom_management.js
@@ -0,0 +1,37 @@
+const historicContent = document.getElementById('historic-content');
+const textarea = document.getElementById('textarea1');
+const textareaContainer = document.getElementById('textarea_container');
+const nodeContainer = document.getElementById('test');
+
+const createChatBox = (elt, elt2, br, valueToSave, parent1) => {
+    const para = document.createElement(elt);
+    const span = document.createElement(elt2); //create main element
+    const breakLine = document.createElement(br); // add a breakline
+    const node = document.createTextNode(valueToSave); // write the text
+    span.appendChild(node); // insert the text into main element
+    para.appendChild(span);
+    parent1.appendChild(para); // insert the span element with text in the parent element
+    parent1.appendChild(breakLine); // insert the breakline in the parent element
+    return para // return the last paragraphe element created
+};
+
+
+const removeClass = (eltToModify, classToRemove) => {
+    eltToModify.classList.remove(classToRemove);
+};
+
+const addClass = (eltToModify, classesToAdd=[]) => {
+    for (classToAdd of classesToAdd) {
+        eltToModify.classList.add(classToAdd);
+    }
+};
+
+const historicCreate = textarea.addEventListener("keyup", (e) => {
+    if (e.keyCode === 13) {
+        const historicElt = createChatBox("p", "span", 'br', textarea.value, historicContent);
+        addClass(historicElt, ['text-justify']);
+        // addClass(historicElt.childNodes, ['white-text'])
+        removeClass(textareaContainer, 'invisible');
+        textarea.value = ""
+    }
+});
