26cc29887656bfd3494486f44851a56ad9368354
youpiyoful <yoanfornari@gmail.com>
2020-04-06 09:34:27 +0200

    [wip] use pylint for best practice

--- grandpyapp/parser.py
+++ grandpyapp/parser.py
@@ -7,12 +7,21 @@ class Parser:
         self.answer = answer
         self.stop_words = stop_words
 
+    @staticmethod
+    def another_public_method():
+        """an other public method"""
+        return "an other public method"
+
     def find_keyword(self):
         """this fuction take a string and return a list of keyword"""
         list_of_words = self.answer.split(" ")
 
         for stop_word in self.stop_words:
 
-            [list_of_words.remove(word) for word in list_of_words if word == stop_word]  # use list comprehension
-                    
+            # use list comprehension
+            [list_of_words.remove(word) for word in list_of_words if word == stop_word]
+
         return list_of_words
+
+# parser = Parser("ceci est ma phrase", ["ceci", "est", "ma"])
+# print(parser.find_keyword())
