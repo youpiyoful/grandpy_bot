0ea8c034c5442b40e34e7c8448b25ab7fcb20415
youpiyoful <yoanfornari@gmail.com>
2020-04-07 18:10:07 +0200

    [fix] the response of requests is mock

--- grandpyapp/tests/test_place.py
+++ grandpyapp/tests/test_place.py
@@ -10,27 +10,29 @@ PLACE = place.Place(["openclassrooms"], GOOGLE_API_KEY, BASE_URL_GOOGLE_PLACE)
 
 def test_find_place(monkeypatch):
     """this test verify than method return a correct json"""
-    google_resp = {
+    google_resp = requests.models.Response()
+    google_resp.code = "200"
+    google_resp.json = """{
         "candidates": [
             {
                 "formatted_address": "7 Cité Paradis, 75010 Paris, France",
-                "geometry": {
-                    "lat": 48.8748465,
-                    "long": 2.3504873
-                }
+                "name": "OpenClassrooms"
             }
-        ]}
+        ],
+        "status": "OK"
+    }"""
 
-    def mock_requests():
-        return json.dumps(google_resp)
+    def mock_req(request):
+        return google_resp
 
-    monkeypatch.setattr(place, 'requests', mock_requests)
+
+    monkeypatch.setattr(requests.Response, 'json', mock_req)
 
     resp = PLACE.find_place()
-    print('response = ', resp)
-    # print('google_resp = ', google_resp["candidates"][0]["formatted_address"])
-    assert resp == requests.get(f"{BASE_URL_GOOGLE_PLACE}?input=openclassrooms&inputtype=textquery&fields=formatted_address,name&key={GOOGLE_API_KEY}")
-
+    print('google_resp = ', json.loads(google_resp.json))
+    print('response = ', json.loads(resp))
+    assert json.loads(resp) == json.loads(google_resp.json)
+# 
 
 def test_build_url_for_api_place():
     """this method take Place's attribut and build an endpoint"""
