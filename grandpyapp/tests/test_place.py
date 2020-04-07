efc100f50cba42f53498d2a5e33818402728b63a
youpiyoful <yoanfornari@gmail.com>
2020-04-07 09:26:43 +0200

    [issue] have an issue with mock_requests

--- grandpyapp/tests/test_place.py
+++ grandpyapp/tests/test_place.py
@@ -1,14 +1,16 @@
 """This file test the file class place than use the google place api"""
 from grandpyapp import place
+from config import GOOGLE_API_KEY, BASE_URL_GOOGLE_PLACE
 import requests
+import json
 
-PLACE = place.Place("openclassrooms", "anApiKey", "http://ça_marche_fort_bien.com")
 
+# PLACE = place.Place(["openclassrooms"], "anApiKey", "http://ça_marche_fort_bien.com")
+PLACE = place.Place(["openclassrooms"], GOOGLE_API_KEY, BASE_URL_GOOGLE_PLACE)
 
 def test_find_place(monkeypatch):
     """this test verify than method return a correct json"""
-    google_resp = \
-    {
+    google_resp = {
         "candidates": [
             {
                 "formatted_address": "7 Cité Paradis, 75010 Paris, France",
@@ -17,20 +19,19 @@ def test_find_place(monkeypatch):
                     "long": 2.3504873
                 }
             }
-        ]
-    }
+        ]}
 
-    def mock_google_api_request(request):
-        return google_resp
+    def mock_requests():
+        return json.dumps(google_resp)
 
-    monkeypatch.setattr(place, 'une fonction de request', mock_google_api_request)
+    monkeypatch.setattr(place, 'requests', mock_requests)
 
     resp = PLACE.find_place()
     print('response = ', resp)
-    print('google_resp = ', google_resp["candidates"][0]["formatted_address"])
-    # assert google_resp["candidates"][0]["formatted_address"] == response["candidates"][0]["formatted_address"]
+    # print('google_resp = ', google_resp["candidates"][0]["formatted_address"])
+    assert resp == requests.get(f"{BASE_URL_GOOGLE_PLACE}?input=openclassrooms&inputtype=textquery&fields=formatted_address,name&key={GOOGLE_API_KEY}")
 
 
 def test_build_url_for_api_place():
     """this method take Place's attribut and build an endpoint"""
-    PLACE
+    assert PLACE.build_url_for_api_place() == f"{BASE_URL_GOOGLE_PLACE}?input=openclassrooms&inputtype=textquery&fields=formatted_address,name&key={GOOGLE_API_KEY}"
