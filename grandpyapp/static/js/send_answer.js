9eb15fbda1afc61917e6e6ecbaa193ab425985a5
youpiyoful <yoanfornari@gmail.com>
2020-04-07 09:27:22 +0200

    [wip] add an local url for test

--- grandpyapp/static/js/send_answer.js
+++ grandpyapp/static/js/send_answer.js
@@ -2,7 +2,7 @@ const form = document.getElementById('send');
 
 const sendForm = () => {
     const request = new XMLHttpRequest();
-    request.open('GET', 'http://127.0.0.1:5000/')
+    request.open('GET', 'http://127.0.0.1:5000/send_answer/<answer>')
 }
 form.addEventListener("click", (event) => {
     
