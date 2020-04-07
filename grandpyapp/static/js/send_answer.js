4a25a3b9315b61be0dd83fc4052ed75271b6a853
youpiyoful <yoanfornari@gmail.com>
2020-04-07 12:22:45 +0200

    [wip] work on response of xmlhttprequest

--- grandpyapp/static/js/send_answer.js
+++ grandpyapp/static/js/send_answer.js
@@ -1,9 +1,18 @@
-const form = document.getElementById('send');
+// const submit = document.getElementById('send');
+const textarea = document.getElementById('textarea');
+const textareaValue = textarea.value;
 
-const sendForm = () => {
+const getResponseToAnswer = () => {
     const request = new XMLHttpRequest();
-    request.open('GET', 'http://127.0.0.1:5000/send_answer/<answer>')
+    const url = 'http://127.0.0.1:5000/send_answer/' + textareaValue;
+    console.log(url);
+    request.open('GET', url);
+    return request.response
 }
-form.addEventListener("click", (event) => {
-    
+
+textarea.addEventListener("keydown", (event) => {
+    if (event.keyCode == 13) {
+        const response = getResponseToAnswer();
+        console.log(response)
+    }
 })
