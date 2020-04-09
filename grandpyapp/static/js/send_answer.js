2204d41edac292ae2d50597d83a7d47d68629d4e
youpiyoful <yoanfornari@gmail.com>
2020-04-09 16:37:32 +0200

    [comment] make this file in standby for eventual delete

--- grandpyapp/static/js/send_answer.js
+++ grandpyapp/static/js/send_answer.js
@@ -1,21 +1,28 @@
-// const submit = document.getElementById('send');
+const historicContent = document.getElementById('historic-content');
 const textareaToSubmit = document.getElementById('textarea');
 const textareaToSubmitValue = textareaToSubmit.value;
-// const baseUrl = 'http://127.0.0.1:5000/send_answer/'
 
-const getResponseToAnswer = () => {
-    const request = new XMLHttpRequest();
-    // const url = baseUrl + textareaToSubmitValue;
-    // console.log(url);
-    request.open('GET', 'http://127.0.0.1:5000/send_answer/' + textareaToSubmitValue);
-    console.log(request.responseType)
-    console.log(request.responseText)
-    return request.response
-}
+// fetch('')
+// const getResponseToAnswer = () => {
+//     const request = new XMLHttpRequest();
+//     // const url = baseUrl + textareaToSubmitValue;
+//     // console.log(url);
+//     request.open('GET', 'http://127.0.0.1:5000/send_answer/' + textareaToSubmitValue);
+//     console.log(request.responseType)
+//     console.log(request.responseText)
+//     return request.response
+// }
 
-textarea.addEventListener("keydown", (event) => {
-    if (event.keyCode == 13) {
-        const response = getResponseToAnswer();
-        console.log(response)
-    }
-})
+
+
+// textarea.addEventListener("keydown", (event) => {
+//     if (event.keyCode == 13) {
+//         fetch('http://127.0.0.1:5000/send_answer/' + textareaToSubmitValue)
+//         .then(response => response.json())
+//         .then(data => {
+//           console.log(data) // Prints result from `response.json()` in getRequest
+//         })
+//         .catch(error => console.error(error))
+//     }
+// })
+
