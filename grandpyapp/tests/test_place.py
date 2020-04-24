a4c7ab8746d2c8da827825f118f067989e37d62c
youpiyoful <yoanfornari@gmail.com>
2020-04-24 19:12:33 +0200

    [refactoring] create a class MockResponse for mock a real json object

--- grandpyapp/tests/test_place.py
+++ grandpyapp/tests/test_place.py
@@ -1,39 +1,35 @@
 """This file test the file class place than use the google place api"""
 from grandpyapp import place
-from config import GOOGLE_API_KEY, BASE_URL_GOOGLE_PLACE
+# from config import GOOGLE_API_KEY, BASE_URL_GOOGLE_PLACE
 import requests
 import json
 
+DEFAULT_CONFIG = {"google_api_key": "apikey", "base_url_google_place": "https://fakeurl"}
+PLACE = place.QueryPlace(["openclassrooms"], DEFAULT_CONFIG['google_api_key'], DEFAULT_CONFIG['base_url_google_place'])
 
-# PLACE = place.Place(["openclassrooms"], "anApiKey", "http://ça_marche_fort_bien.com")
-PLACE = place.Place(["openclassrooms"], GOOGLE_API_KEY, BASE_URL_GOOGLE_PLACE)
+
+class MockResponse:
+
+    @staticmethod
+    def json():
+        return {"mock_key": "mock_response"}
 
 def test_find_place(monkeypatch):
     """this test verify than method return a correct json"""
-    google_resp = requests.models.Response()
-    google_resp.code = "200"
-    google_resp.json = """{
-        "candidates": [
-            {
-                "formatted_address": "7 Cité Paradis, 75010 Paris, France",
-                "name": "OpenClassrooms"
-            }
-        ],
-        "status": "OK"
-    }"""
 
-    def mock_req(request):
-        return google_resp
+    def mock_get(*args, **kwargs):
+        return MockResponse()
 
+    monkeypatch.setattr(requests, "get", mock_get)
 
-    monkeypatch.setattr(requests.Response, 'json', mock_req)
 
     resp = PLACE.find_place()
-    print('google_resp = ', json.loads(google_resp.json))
-    print('response = ', json.loads(resp))
-    assert json.loads(resp) == json.loads(google_resp.json)
-# 
+    print('response = ', resp)
+    assert resp["mock_key"] == "mock_response"
+
 
 def test_build_url_for_api_place():
     """this method take Place's attribut and build an endpoint"""
-    assert PLACE.build_url_for_api_place() == f"{BASE_URL_GOOGLE_PLACE}?input=openclassrooms&inputtype=textquery&fields=formatted_address,name&key={GOOGLE_API_KEY}"
+    assert PLACE.build_url_for_api_place() == \
+    f"{DEFAULT_CONFIG['base_url_google_place']}?input=openclassrooms&inputtype" \
+    f"=textquery&fields=formatted_address,name&key={DEFAULT_CONFIG['google_api_key']}"
