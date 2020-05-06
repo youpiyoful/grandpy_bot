e9dbd9dab92932e1cea0aacb4351f7d2af2bf6b5
youpiyoful <yoanfornari@gmail.com>
2020-05-06 04:28:02 +0200

    [refactoring] i refactoring my test with my new knowledge, and i rewrite the import for more pythonic code

--- grandpyapp/tests/test_place.py
+++ grandpyapp/tests/test_place.py
@@ -1,31 +1,32 @@
 """This file test the file class place than use the google place api"""
-from grandpyapp import place
+from grandpyapp.place import QueryPlace
 # from config import GOOGLE_API_KEY, BASE_URL_GOOGLE_PLACE
 import requests
 import json
 
 DEFAULT_CONFIG = {"google_api_key": "apikey", "base_url_google_place": "https://fakeurl"}
-PLACE = place.QueryPlace(["openclassrooms"], DEFAULT_CONFIG['google_api_key'], DEFAULT_CONFIG['base_url_google_place'])
+PLACE = QueryPlace(["openclassrooms"], DEFAULT_CONFIG['google_api_key'], DEFAULT_CONFIG['base_url_google_place'])
 
-
-class MockResponse:
-
-    @staticmethod
-    def json():
-        return {"mock_key": "mock_response"}
 
 def test_find_place(monkeypatch):
     """this test verify than method return a correct json"""
+    
+    class MockResponse:
+        def __init__(self):
+            self.status_code = 200
+        
+        def json(self):
+            return {"mock_key": "mock_response"}
 
-    def mock_get(*args, **kwargs):
+    def mock_requests_get(url):
         return MockResponse()
 
-    monkeypatch.setattr(requests, "get", mock_get)
+    monkeypatch.setattr(requests, "get", mock_requests_get)
 
 
     resp = PLACE.find_place()
     print('response = ', resp)
-    assert resp["mock_key"] == "mock_response"
+    assert resp.json().get("mock_key") == "mock_response"
 
 
 def test_build_url_for_api_place():
