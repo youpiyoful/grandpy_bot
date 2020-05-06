f7633ff6df40a7ffe5b009630726e966fdfddda2
youpiyoful <yoanfornari@gmail.com>
2020-05-06 04:27:03 +0200

    [fix] i fix a positiv false, my product code was right but my test code was wrong

--- grandpyapp/tests/test_view.py
+++ grandpyapp/tests/test_view.py
@@ -47,49 +47,44 @@ def test_index(monkeypatch):
     assert index_html == render
 
 
-class MockResponse:
-
-    @staticmethod
-    def google_json():
-        return {'candidates': [{'formatted_address': '7 Cité Paradis, 75010 Paris, France'}]}
-    
-    @staticmethod
-    def wiki_json():
-        return {"je ne connais pas la reponse": "mais je sais qu'elle est en json"}
-
-
 def test_get_data(monkeypatch):
     """test than response wiki return correctly the wiki data when she receive an answer"""
     answer = "Ou se situe openclassrooms"
+    class MockRequestsGet:
+        def __init__(self, url, params=None):
+            self.status_code = 200
+
+        def json(self):
+            return {"candidates": [{"formatted_address": "7 Cité Paradis, 75010 Paris, France"}]}
 
     def mock_find_keyword(*args, **kwargs):
         keyword_from_answer = "openclassrooms"
         return keyword_from_answer
 
     def mock_google_place(*args, **kwargs):
-        return MockResponse().google_json()
+        return MockRequestsGet('https://fakeUrl.com')
 
     def mock_wiki(*args, **kwargs):
-        return MockResponse().wiki_json()
+        return {"je ne connais pas la reponse": "mais je sais qu'elle est en json"}
 
     monkeypatch.setattr(parser.Parser, 'find_keyword', mock_find_keyword)
     monkeypatch.setattr(place.QueryPlace, 'find_place', mock_google_place)
     monkeypatch.setattr(wiki.Wiki, 'find_data_about_place', mock_wiki)
 
-    # response_result = {
-    #     "resp_google":
-    #     {
-    #         "candidates": [
-    #             {
-    #                 "formatted_address": "7 Cité Paradis, 75010 Paris, France",
-    #             }
-    #         ]
-    #     },
-    #     "resp_wiki":
-    #     {
-    #         "je ne connais pas la reponse": "mais je sais qu'elle est en json"
-    #     }
-    # }
+    response_result = {
+        "data_google":
+        {
+            "candidates": [
+                {
+                    "formatted_address": "7 Cité Paradis, 75010 Paris, France",
+                }
+            ]
+        },
+        "data_wiki":
+        {
+            "je ne connais pas la reponse": "mais je sais qu'elle est en json"
+        }
+    }
 
-    print('test = ', view.get_data(answer))
-    assert view.get_data(answer) == {'candidates': [{'formatted_address': '7 Cité Paradis, 75010 Paris, France'}]}
+    response = view.get_data(answer)
+    assert response.json == response_result
