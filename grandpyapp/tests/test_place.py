a9bfa1d66de55dcf21cec1da3968588a7c647f61
youpiyoful <yoanfornari@gmail.com>
2020-05-06 12:43:23 +0200

    [try] i move the mock to a fixture for try this possibilities and it work !

--- grandpyapp/tests/test_place.py
+++ grandpyapp/tests/test_place.py
@@ -1,5 +1,6 @@
 """This file test the file class place than use the google place api"""
 from grandpyapp.place import QueryPlace
+import pytest
 # from config import GOOGLE_API_KEY, BASE_URL_GOOGLE_PLACE
 import requests
 import json
@@ -8,22 +9,26 @@ DEFAULT_CONFIG = {"google_api_key": "apikey", "base_url_google_place": "https://
 PLACE = QueryPlace(["openclassrooms"], DEFAULT_CONFIG['google_api_key'], DEFAULT_CONFIG['base_url_google_place'])
 
 
-def test_find_place(monkeypatch):
-    """this test verify than method return a correct json"""
+class MockResponse:
+    def __init__(self):
+        self.status_code = 200
     
-    class MockResponse:
-        def __init__(self):
-            self.status_code = 200
-        
-        def json(self):
-            return {"mock_key": "mock_response"}
+    @staticmethod
+    def json():
+        return {"mock_key": "mock_response"}
 
-    def mock_requests_get(url):
+
+# monkeypatched requests.get moved to a fixture
+@pytest.fixture
+def mock_response(monkeypatch):
+    """Requests.get() mocked to return {'mock_key': 'mock_response} and status_code == 200"""
+    def mock_requests_get(*args, **kwargs): 
         return MockResponse()
 
     monkeypatch.setattr(requests, "get", mock_requests_get)
 
-
+def test_find_place(mock_response):
+    """this test verify than method return a correct json"""
     resp = PLACE.find_place()
     print('response = ', resp)
     assert resp.json().get("mock_key") == "mock_response"
