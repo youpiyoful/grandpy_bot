d82017e52fff4f1491b36dc08e599f279220a246
youpiyoful <yoanfornari@gmail.com>
2020-05-07 12:11:35 +0200

    [wip] test if any keyword is find in the answer

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,9 +2,18 @@
 from grandpyapp.parser import Parser
 import pytest
 
-PARSER = Parser("ceci est ma phrase", ["ceci", "est", "ma"])
+# PARSER = Parser("ceci est ma phrase", ["ceci", "est", "ma"])
 
 
 def test_find_keyword():
     """test the method who find the keyword in a phrase"""
-    assert PARSER.find_keyword() == ["phrase"]
+    parser = Parser("ceci est ma phrase", ["ceci", "est", "ma"])
+    assert parser.find_keyword() == ["phrase"]
+
+def test_find_any_keyword():
+    """
+    test the method who find the keyword in a phrase
+    return if any keyword is find in the phrase
+    """
+    parser = Parser("ceci est ma phrase", ["ceci", "est", "ma", "phrase"])
+    assert parser.find_keyword() == ["any keyword find"]
