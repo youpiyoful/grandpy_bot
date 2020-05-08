228331d41fec4eebc1705148e7e5ce674d21630c
youpiyoful <yoanfornari@gmail.com>
2020-05-08 04:56:35 +0200

    [add] add negatif test for get_data()

--- grandpyapp/view.py
+++ grandpyapp/view.py
@@ -6,7 +6,8 @@ from grandpyapp.parser import Parser
 
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/grandpybot/*": {"origins": "*"}})
+CORS(app)
+# cors = CORS(app, resources={r"/grandpybot/*": {"origins": "*"}})
 # Config options - Make sure you created a 'config.py' file.
 app.config.from_object('config')
 GOOGLE_API_KEY = app.config.get('GOOGLE_API_KEY')
@@ -26,7 +27,7 @@ def index():
                              reset_css=url_for('static', filename='css/reset.css'),
                              grandpy_app_css=url_for('static', filename='css/grandpy_app.css')), 200)
     # render.set_cookie('same-site-cookie', 'foo', samesite='Lax')
-    render.set_cookie('username', 'the username', samesite='Strict', secure=True)
+    # render.set_cookie('username', 'the username', samesite='Strict', secure=True)
 
     print(render)
     return render
@@ -38,11 +39,20 @@ def get_data(answer):
     stop_words = app.config.get('STOP_WORDS')  # use list of stop word stock in config.py
     parser_obj = Parser(answer, stop_words)  # instanciate Parser class
     keyword = parser_obj.find_keyword()  # call method of PARSER object
+    print("KEYWORD : ", keyword)
+    
+    if keyword[0] == "any keyword find":
+        response = app.response_class(
+            response={"response": "aucun mot clef valide"},
+            status=404,
+            mimetype="application/json"
+        )
+        return response
+
     place_obj = QueryPlace(keyword, GOOGLE_API_KEY, BASE_URL_GOOGLE_PLACE)  # instanciate Place class
     data_google = place_obj.find_place()  # call method of PLACE object
     wiki_obj = Wiki('fr', keyword)  # instanciate wiki class
     data_wiki = wiki_obj.find_data_about_place()  # call method of WIKI object
-    # print(json.dumps(data_wiki.json()))
     data = {"data_google": data_google.json(), "data_wiki": data_wiki} # concatenate data_google and data_wiki in a big json object
     print(data)
     print(type(json.dumps(data)))
--- tests/test_view.py
+++ tests/test_view.py
@@ -3,6 +3,7 @@ from grandpyapp import view, parser, place, wiki
 import pytest
 import json
 
+#region test for index_render_template
 def test_index_render_template_correctly(monkeypatch):
     """test index function return correctly html"""
     index_html = """
@@ -45,31 +46,55 @@ def test_index_render_template_correctly(monkeypatch):
 
     render = view.index()
     assert index_html == render
+#endregion
+
+#region test for get_data
+
+#region mocks
+class MockRequestsGet:
+    def __init__(self, url, params=None):
+        self.status_code = 200
+
+    def json(self):
+        return {"candidates": [{"formatted_address": "7 Cité Paradis, 75010 Paris, France"}]}
 
 
-def test_get_data(monkeypatch):
-    """test than response wiki return correctly the wiki data when she receive an answer"""
-    answer = "Ou se situe openclassrooms"
-    class MockRequestsGet:
-        def __init__(self, url, params=None):
-            self.status_code = 200
-
-        def json(self):
-            return {"candidates": [{"formatted_address": "7 Cité Paradis, 75010 Paris, France"}]}
+@pytest.fixture
+def mock_response_find_keyword_is_ok(monkeypatch):
+    """mock the method find_keyword"""
 
     def mock_find_keyword(*args, **kwargs):
         keyword_from_answer = "openclassrooms"
         return keyword_from_answer
 
+    monkeypatch.setattr(parser.Parser, 'find_keyword', mock_find_keyword)
+
+
+@pytest.fixture
+def mock_response_find_place_is_ok(monkeypatch):
+    """mock the method find_place()"""
     def mock_google_place(*args, **kwargs):
         return MockRequestsGet('https://fakeUrl.com')
+
+    monkeypatch.setattr(place.QueryPlace, 'find_place', mock_google_place)
+
+
+@pytest.fixture
+def mock_response_find_data_about_place_is_ok(monkeypatch):
+    """mock the method find_data_about_place"""
 
     def mock_wiki(*args, **kwargs):
         return {"je ne connais pas la reponse": "mais je sais qu'elle est en json"}
 
-    monkeypatch.setattr(parser.Parser, 'find_keyword', mock_find_keyword)
-    monkeypatch.setattr(place.QueryPlace, 'find_place', mock_google_place)
     monkeypatch.setattr(wiki.Wiki, 'find_data_about_place', mock_wiki)
+
+#endregion
+
+def test_get_data_is_ok(mock_response_find_keyword_is_ok, 
+                        mock_response_find_place_is_ok,
+                        mock_response_find_data_about_place_is_ok):
+    """test than response wiki return correctly the wiki data when she receive an answer"""
+    answer = "Ou se situe openclassrooms"
 
     response_result = {
         "data_google":
@@ -88,3 +113,35 @@ def test_get_data(monkeypatch):
 
     response = view.get_data(answer)
     assert response.json == response_result
+
+
+def test_get_data_if_any_keyword(mock_response_find_place_is_ok, 
+                                 mock_response_find_data_about_place_is_ok,
+                                 monkeypatch):
+    """test the behavior of get_data() if parser.find_keyword return any keyword"""
+    answer = ""
+    # class MockResponse():
+    #     def __init__(self):
+    #         self.status_code = 404
+    #         self.response = json.dumps({"response": "aucun mot clef valide"})
+    #         self.mimetype = {}
+    def mock_find_any_keyword(*args, **kwargs):
+        return ["any keyword find"]
+    
+    monkeypatch.setattr(parser.Parser, 'find_keyword', mock_find_any_keyword)
+
+    response = view.get_data(answer)
+
+    assert response.response == {"response": "aucun mot clef valide"}
+    assert response.status_code == 404
+
+
+def test_get_data_if_google_place_is_wrong():
+    """test the behavior of get_data() if google_place encounter a problem"""
+    pass
+
+def test_get_data_if_wikipedia_is_wrong():
+    """test the behavior of get_data() if find_data_about_place() encounter a problem"""
+    pass
+
+#endregion
