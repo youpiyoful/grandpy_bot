bbd0e47c28f1780bcd0f03082fa3d25ac70293dc
youpiyoful <yoanfornari@gmail.com>
2020-05-09 01:45:20 +0200

    [add] add test of error for find_data_about_place (wikipedia)

--- tests/test_wiki.py
+++ tests/test_wiki.py
@@ -18,3 +18,20 @@ def test_find_data_about_place(monkeypatch):
     resp = wiki.find_data_about_place()
     assert resp == {"wiki_response": "Openclassrooms est un site web d'apprentissage."}
     assert resp.get('wiki_response') == "Openclassrooms est un site web d'apprentissage."
+
+
+def test_find_any_data_about_place(monkeypatch):
+    """
+    test the behavior of the find_data_about_place method when the keywords
+    in parameters cannot be found
+    """
+    from wikipedia import exceptions
+    page_error_response = {'error': 'Page id "tour eifel grandes" does not match any pages. Try another id!'}
+
+    def mock_wikipedia_page_error(*args, **kwargs):
+        raise wikipedia.exceptions.PageError('tour eifel grandes')
+    
+    monkeypatch.setattr(wikipedia, 'summary', mock_wikipedia_page_error)
+
+    resp_wrong = wiki.find_data_about_place()
+    assert resp_wrong == page_error_response
