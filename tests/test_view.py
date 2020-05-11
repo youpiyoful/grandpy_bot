471999488082643965f1ad0991d421f3aea945e2
youpiyoful <yoanfornari@gmail.com>
2020-05-11 02:59:21 +0200

    [wip] i try to request.args with a class MockRequest

--- tests/test_view.py
+++ tests/test_view.py
@@ -59,6 +59,45 @@ class MockRequestsGet:
         return {"candidates": [{"formatted_address": "7 Cité Paradis, 75010 Paris, France"}]}
 
 
+class MockRequest:
+    def __init__(self, *args):
+        last = []
+        for arg in args:
+            last += [(x, y) for x, y in arg.items()]
+
+        self.args = dict((x, y) for x, y in last)
+
+            
+    # def get(self, key_arg):
+
+    #     for arg in self.args:
+    #         dict_arg = [(key, value) for key, value in arg.items()][0]
+        
+    #         if key_arg == dict_arg[0]:
+    #             return dict_arg[1]
+
+    #     return None
+
+
+# r = MockRequest({"test": "test"}, {"test2": "test2"})
+# print(r.args)
+# print(r.args.get("test"))
+# print(r.get("test"))
+
+# print("}, {".split(str(r.args)))
+        
+
+
+@pytest.fixture
+def mock_request_args_is_ok(monkeypatch):
+    """mock the request.args"""
+
+    def mock_request_args():
+        return MockRequest(dict(answer="ou se trouve la tour eifel ?"))
+
+    monkeypatch.setattr(view.get_data.request, 'request.args', mock_request_args)
+ 
+
 @pytest.fixture
 def mock_response_find_keyword_is_ok(monkeypatch):
     """mock the method find_keyword"""
@@ -73,10 +112,10 @@ def mock_response_find_keyword_is_ok(monkeypatch):
 @pytest.fixture
 def mock_response_find_place_is_ok(monkeypatch):
     """mock the method find_place()"""
-    def mock_google_place(*args, **kwargs):
+    def mock_find_place(*args, **kwargs):
         return MockRequestsGet('https://fakeUrl.com')
 
-    monkeypatch.setattr(place.QueryPlace, 'find_place', mock_google_place)
+    monkeypatch.setattr(place.QueryPlace, 'find_place', mock_find_place)
 
 
 @pytest.fixture
@@ -84,7 +123,7 @@ def mock_response_find_data_about_place_is_ok(monkeypatch):
     """mock the method find_data_about_place"""
 
     def mock_wiki(*args, **kwargs):
-        return {"je ne connais pas la reponse": "mais je sais qu'elle est en json"}
+        return {"i don't know the response": "but i know it's in json"}
 
     monkeypatch.setattr(wiki.Wiki, 'find_data_about_place', mock_wiki)
 
@@ -92,9 +131,10 @@ def mock_response_find_data_about_place_is_ok(monkeypatch):
 
 def test_get_data_is_ok(mock_response_find_keyword_is_ok, 
                         mock_response_find_place_is_ok,
-                        mock_response_find_data_about_place_is_ok):
+                        mock_response_find_data_about_place_is_ok,
+                        mock_request_args_is_ok):
     """test than response wiki return correctly the wiki data when she receive an answer"""
-    answer = "Ou se situe openclassrooms"
+    # answer = "Ou se situe openclassrooms"
 
     response_result = {
         "data_google":
@@ -107,16 +147,17 @@ def test_get_data_is_ok(mock_response_find_keyword_is_ok,
         },
         "data_wiki":
         {
-            "je ne connais pas la reponse": "mais je sais qu'elle est en json"
+            "i don't know the response": "but i know it's in json"
         }
     }
 
-    response = view.get_data(answer)
+    response = view.get_data()
     assert response.json == response_result
 
 
 def test_get_data_if_any_keyword(mock_response_find_place_is_ok, 
                                  mock_response_find_data_about_place_is_ok,
+                                 mock_request_args_is_ok,
                                  monkeypatch):
     """test the behavior of get_data() if parser.find_keyword return any keyword"""
     def mock_find_any_keyword(*args, **kwargs):
@@ -124,18 +165,26 @@ def test_get_data_if_any_keyword(mock_response_find_place_is_ok,
     
     monkeypatch.setattr(parser.Parser, 'find_keyword', mock_find_any_keyword)
 
-    response = view.get_data("")
+    response = view.get_data()
 
     assert response.json == {"error": "aucun mot clef valide"}
     assert response.status_code == 404
 
 
-def test_get_data_if_google_place_is_wrong():
+def test_get_data_if_google_place_is_wrong(
+    mock_response_find_keyword_is_ok, 
+    mock_response_find_data_about_place_is_ok,
+    monkeypatch):
     """test the behavior of get_data() if google_place encounter a problem"""
+    
+    def mock_find_place_is_wrong():
+        pass
+
     pass
 
 def test_get_data_if_wikipedia_is_wrong(mock_response_find_place_is_ok,
                                         mock_response_find_keyword_is_ok,
+                                        mock_request_args_is_ok,
                                         monkeypatch):
     """test the behavior of get_data() if find_data_about_place() encounter a problem"""
 
@@ -144,7 +193,7 @@ def test_get_data_if_wikipedia_is_wrong(mock_response_find_place_is_ok,
     
     monkeypatch.setattr(wiki.Wiki, 'find_data_about_place', mock_find_any_data_about_place)
 
-    resp = view.get_data('tour eifel')
+    resp = view.get_data()
     assert resp.status_code == 404
     assert resp.json == {'error': 'aucune donnée trouvée'}
 
