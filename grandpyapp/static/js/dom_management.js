fae82db011fb166be3b55aa7e5a9a7f900258a36
youpiyoful <yoanfornari@gmail.com>
2020-05-22 04:48:06 +0200

    [wip] add the control response, 200 or not and built a response

--- grandpyapp/static/js/dom_management.js
+++ grandpyapp/static/js/dom_management.js
@@ -26,41 +26,63 @@ const addClass = (eltToModify, classesToAdd=[]) => {
 
 const historicCreate = textarea.addEventListener("keyup", (e) => {
     if (e.keyCode === 13) {
+        const valueOfAnswer = textarea.value;
         const historicElt = createChatBox("p", textarea.value, historicContent);
-        let valueOfAnswer = textarea.value
-        textarea.value = ''
+        textarea.value = '';
         const p = historicElt;
         addClass(p, ['chatbox-text-wrap', 'chatbox-p', 'blue-text']);
         removeClass(historicContent, 'invisible');
-        fetch('http://127.0.0.1:5000/send_answer?answer=' + valueOfAnswer, {
-            method: 'GET'
-            // body: json
-        })
-        .then(response => {console.log(response); return response.json()})
-        .then(data => {
-            console.log("data => ", data) // Prints result from `response.json()` in getRequest
-             
-            if (data.data_google.candidates[0]) {
-                data_text = "Avant toute chose je tiens à préciser que je suis un peu sénile... \
-                Le lieu nommé " + data.data_google.candidates[0].name + " se trouve " + data.data_google.candidates[0].formatted_address + "."
-                if (data.data_wiki.wiki_response) {
-                    data_text_wiki = " Tient j'oubliais, est ce que tu savais que " + data.data_wiki.wiki_response
+        console.log("VALUE OF ANSWER : ", valueOfAnswer);
+        console.log("if value", /[A-z]/.test(valueOfAnswer));
+        if (/[A-z]/.test(valueOfAnswer)){
+            console.log("value of answer is not null");
+            fetch('http://127.0.0.1:5000/send_answer?answer=' + valueOfAnswer, {
+                method: 'GET'
+                // body: json
+            })
+            .then(response => {
+                console.log(response);
+                if (response.status == 200) return response.json(); else return response.status;
+            })
+            .then(data => {
+                console.log("data => ", data); // Prints result from `response.json()` in getRequest
+                console.log("type of data => ", typeof(data));
+                if (typeof(data) != "number" && data.data_google.candidates[0]) {
+                    data_text = "Avant toute chose je tiens à préciser que je suis un peu sénile... \
+                    Le lieu nommé " + data.data_google.candidates[0].name + " se trouve " + data.data_google.candidates[0].formatted_address + "."
+                    if (data.data_wiki.wiki_response) {
+                        data_text_wiki = " Tient j'oubliais, est ce que tu savais que " + data.data_wiki.wiki_response
+                    }
+                    else {
+                        data_text_wiki = " Oups, j'ai rien a te dire à ce sujet... Même moi je ne sais pas tout !";
+                    }
+                } else {
+                    data_text = "La sénilité me guette ! J'ai rien trouvé p'tit";
+                    data_text_wiki = "";
                 }
-            } else {
-                data_text = "La sénilité me guette ! J'ai rien trouvé p'tit"
-            }
 
-            console.log(data_text)
-            const historicElt1 = createChatBox("p", data_text, historicContent);
-            const historicElt2 = createChatBox("p", data_text_wiki, historicContent);
-            const p = historicElt1;
-            const p2 = historicElt2;
+                console.log("data_text =", data_text)
+                const historicElt1 = createChatBox("p", data_text, historicContent);
+                const p = historicElt1;
+                addClass(p, ['chatbox-text-wrap', 'chatbox-p', 'green-text']);
+                if (data_text_wiki){
+                    const historicElt2 = createChatBox("p", data_text_wiki, historicContent);
+                    const p2 = historicElt2;
+                    addClass(p2, ['chatbox-text-wrap', 'chatbox-p', 'green-text']);
+                }
+                
+            })
+            .catch(error => console.error("error => ", error))
+        } else {
+            console.log("value of answer is null");
+            const textIsEmpty = createChatBox("p", "Ne soyez pas si timide !", historicContent);
+            const p = textIsEmpty;
             addClass(p, ['chatbox-text-wrap', 'chatbox-p', 'green-text']);
-            addClass(p2, ['chatbox-text-wrap', 'chatbox-p', 'green-text']);
-        })
-        .catch(error => console.error("error => ", error))
+        }
     }
 });
+
+// TODO: gérer les cas ou wiki ou google ne retourne rien
 
 // textarea.addEventListener("keyup", (event) => {
 //     if (event.keyCode == 13) {
