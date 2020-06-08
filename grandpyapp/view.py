ed760fb5552dc58bc50f1716c70ba9c1ee599bc0
youpiyoful <yoanfornari@gmail.com>
2020-06-08 12:03:37 +0200

    [refactoring] use pylint for best practice

--- grandpyapp/view.py
+++ grandpyapp/view.py
@@ -1,35 +1,37 @@
 """This page contains all the endpoints of our application"""
-from flask import Flask, render_template, url_for, json, make_response, request, jsonify
+from flask import Flask, render_template, url_for, json, make_response, request
 from flask_cors import CORS
 from grandpyapp.place import QueryPlace
 from grandpyapp.wiki import Wiki
 from grandpyapp.parser import Parser
 
-app = Flask(__name__)
-CORS(app)
-# cors = CORS(app, resources={r"/grandpybot/*": {"origins": "*"}})
+APP = Flask(__name__)
+CORS(APP)
+# cors = CORS(APP, resources={r"/grandpybot/*": {"origins": "*"}})
 # Config options - Make sure you created a 'config.py' file.
-app.config.from_object('config.DevelopmentConfig')
-# app.config.from_object('config.ProductionConfig')
-GOOGLE_API_KEY = app.config.get('GOOGLE_API_KEY')
-BASE_URL_GOOGLE_PLACE = app.config.get('BASE_URL_GOOGLE_PLACE')
-# BASE_URL_WIKI = app.config.get('BASE_URL_WIKI')
+APP.config.from_object('config.DevelopmentConfig')
+# APP.config.from_object('config.ProductionConfig')
+GOOGLE_API_KEY = APP.config.get('GOOGLE_API_KEY')
+BASE_URL_GOOGLE_PLACE = APP.config.get('BASE_URL_GOOGLE_PLACE')
+# BASE_URL_WIKI = APP.config.get('BASE_URL_WIKI')
 
 
-@app.route('/', methods=['GET'])
-@app.route('/grandpy_bot/', methods=['GET'])
+@APP.route('/', methods=['GET'])
+@APP.route('/grandpy_bot/', methods=['GET'])
 def index():
     """This function returns the single page of the app"""
     # username = request.cookies.get('username')
     render = make_response(render_template('index.html',
                                            api_key=GOOGLE_API_KEY,
                                            #  key_word_place="openclassrooms",
-                                           # logo is dither and take 80% space in less (low tech mag)
+                                           # logo is dither it take 80% space in less (low tech mag)
                                            logo=url_for(
                                                'static', filename='img/dither_it_logo.jpg'),
                                            reset_css=url_for(
                                                'static', filename='css/reset.css'),
-                                           grandpy_app_css=url_for('static', filename='css/grandpy_app.css')), 200)
+                                           grandpy_app_css=url_for(
+                                               'static', filename='css/grandpy_app.css'
+                                               )), 200)
     # render.set_cookie('same-site-cookie', 'foo', samesite='Lax')
     # render.set_cookie('username', 'the username', samesite='Strict', secure=True)
 
@@ -37,7 +39,7 @@ def index():
     return render
 
 
-@app.route('/send_answer', methods=['POST', 'GET'])
+@APP.route('/send_answer', methods=['POST', 'GET'])
 def get_data():
     """This function take an answer and return a json with api google place and api wiki response"""
     print("APIKEY : ", GOOGLE_API_KEY)
@@ -45,7 +47,7 @@ def get_data():
     print("answer from get_data() : ", answer)
     status_code = 200  # if no error is raised the status code remains equal to 200
     # use list of stop word stock in config.py
-    stop_words = app.config.get('STOP_WORDS')
+    stop_words = APP.config.get('STOP_WORDS')
     print('ANSWER from view.get_data() : ', answer)
     parser_obj = Parser(answer, stop_words)  # instanciate Parser class
     keywords = parser_obj.find_keyword()  # call method of PARSER object
@@ -62,7 +64,7 @@ def get_data():
         wiki_obj = Wiki('fr', keywords)  # instanciate wiki class
         data_wiki = wiki_obj.find_data_about_place()  # call method of WIKI object
 
-        if [i for i in data_wiki][0] == 'error':
+        if 'error' in data_wiki:
             data = None
             status_code = 404
 
@@ -71,7 +73,7 @@ def get_data():
             data = {"data_google": data_google.json(), "data_wiki": data_wiki}
             print(data)
 
-    response = app.response_class(
+    response = APP.response_class(
         response=json.dumps(data, ensure_ascii=False),
         status=status_code,
         mimetype='application/json'
