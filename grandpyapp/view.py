97176682868e501ef8a893844e5664d84b486ffb
youpiyoful <yoanfornari@gmail.com>
2020-06-10 21:37:19 +0200

    [refactoring] now get_data is simple and all the logic has been outsourced

--- grandpyapp/view.py
+++ grandpyapp/view.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from grandpyapp.place import QueryPlace
 from grandpyapp.wiki import Wiki
 from grandpyapp.parser import Parser
+from grandpyapp.grandpy_response import GrandpyappResponse
 
 APP = Flask(__name__)
 CORS(APP)
@@ -42,39 +43,17 @@ def get_data():
     This function take an answer and return a json
     with api google place and api wiki response
     """
-    print("APIKEY : ", GOOGLE_API_KEY)
     answer = request.args.get("answer")
-    print("answer from get_data() : ", answer)
     # if no error is raised the status code remains equal to 200
     status_code = 200
     # use list of stop word stock in config.py
     stop_words = APP.config.get("STOP_WORDS")
-    print("ANSWER from view.get_data() : ", answer)
-    parser_obj = Parser(answer, stop_words)  # instanciate Parser class
-    keywords = parser_obj.find_keyword()  # call method of PARSER object
-    print("KEYWORD : ", keywords)
-
-    if keywords[0] == "any keyword find":
-        status_code = 404
-        data = {"error": "aucun mot clef valide"}
-
-    else:
-        # instanciate Place class
-        place_obj = QueryPlace(keywords, GOOGLE_API_KEY, BASE_URL_GOOGLE_PLACE)
-        data_google = place_obj.find_place()  # call method of PLACE object
-        wiki_obj = Wiki("fr", keywords)  # instanciate wiki class
-        # call method of WIKI object
-        data_wiki = wiki_obj.find_data_about_place()
-
-        if "error" in data_wiki:
-            data = None
-            status_code = 404
-
-        else:
-            # concatenate data_google and data_wiki in a big json object
-            data = {"data_google": data_google.json(), "data_wiki": data_wiki}
-            print(data)
-
+    grandpy_resp = GrandpyappResponse(
+        stop_words, answer, GOOGLE_API_KEY, BASE_URL_GOOGLE_PLACE
+    )
+    build_reponse = grandpy_resp.build_response_of_grandpy()
+    data = build_reponse.get("response")
+    status_code = build_reponse.get("status")
     response = APP.response_class(
         response=json.dumps(data, ensure_ascii=False),
         status=status_code,
